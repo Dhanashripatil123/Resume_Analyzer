51b03c583cdc0b205286fa89f3c4ebaae8c9537a
dhanashri <bdhffkfkv@gamal.com>
2026-05-24 01:49:15 +0530

    first commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,10 +5,9 @@ const cors = require('cors');
 
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = [process.env.FRONTEND_URL || "https://resume-analyzer-frontend-itr2.onrender.com"];
-
-app.use(cors({
-     origin: function (origin, callback) {
+const allowedOrigins = [[process.env.FRONTEND_URL || "https://resume-analyzer-frontend-itr2.onrender.com"]];
+const corsOptions = {
+  origin: function (origin, callback) {
           // allow requests with no origin like mobile apps or curl
           if (!origin) return callback(null, true);
           if (allowedOrigins.indexOf(origin) !== -1) {
@@ -17,7 +16,8 @@ app.use(cors({
           return callback(new Error("CORS policy: Origin not allowed"));
      },
      credentials: true,
-}));
+};
+app.use(cors(corsOptions));
 
 // require all the route here
 const authRouter = require("./routes/auth.routes")
