c45db89427c68bdbcbcca666f0838c3627efd9b4
dhanashri <bdhffkfkv@gamal.com>
2026-05-23 23:40:59 +0530

    Fix pdf port issue

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,10 @@ connectTOB();
 // Start Server
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
+
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
 });
