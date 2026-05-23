d15156b0c8bdd544e98a7f5146a4b6c8706d1b35
dhanashri <bdhffkfkv@gamal.com>
2026-05-24 01:45:18 +0530

    first commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:5173", "http://localhost:5174", "http://localhost:5175"];
+const allowedOrigins = [process.env.FRONTEND_URL || "https://resume-analyzer-frontend-itr2.onrender.com"];
 
 app.use(cors({
      origin: function (origin, callback) {
