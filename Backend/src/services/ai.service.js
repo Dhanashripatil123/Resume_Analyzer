79f3e27999947f08c542e7f560bf116a19b515d3
dhanashri <bdhffkfkv@gamal.com>
2026-05-29 22:31:24 +0530

    fix puppeteer error

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -119,7 +119,7 @@ ${jobDescription}
   try {
 
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash-lite",
+      model: "gemini-2.5-pro",
       contents: prompt,
       config: {
         temperature: 0,
@@ -210,7 +210,7 @@ The resume should be not be so long, it should ideally fit in 1-2 pages when con
 
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash-lite",
+      model: "gemini-2.5-pro",
       contents: prompt,
       config: {
         temperature: 0,
